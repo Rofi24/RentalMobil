484703b6630c32ced06a2ea7ca9cdf10a87d1108
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-08 21:23:33 +0700

    Menambahkan fungsi composable untuk DetailDestination dan navigasi ke EditDestination

--- app/src/main/java/com/example/rentalmobil/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/rentalmobil/navigation/PengelolaHalaman.kt
@@ -3,14 +3,18 @@ package com.example.rentalmobil.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.rentalmobil.ui.Halaman.DestinasiHome
 import com.example.rentalmobil.ui.Halaman.HomeScreen
 import com.example.rentalmobil.ui.add.AddMobil
 import com.example.rentalmobil.ui.add.DestinasiEntry
 import com.example.rentalmobil.ui.detail.DetailDestination
+import com.example.rentalmobil.ui.detail.DetailMobil
+import com.example.rentalmobil.ui.edit.EditDestination
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
@@ -33,9 +37,27 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         }
         composable(DestinasiEntry.route) {
             AddMobil(navigateBack = {
-                navController.popBackStack()
-            })
-
+                    navController.popBackStack()
+                }
+            )
+        }
+        composable(
+            route = DetailDestination.routeWithArgs,
+            arguments = listOf(navArgument(DetailDestination.mobilId) {
+                type = NavType.StringType
+                }
+            )
+        ) { backStackEntry ->
+            val mobilId = backStackEntry.arguments?.getString(DetailDestination.mobilId)
+            mobilId?.let {
+                DetailMobil(
+                    navigateBack = { navController.popBackStack() },
+                    navigateToEditItem = {
+                        navController.navigate("${EditDestination.route}/$mobilId")
+                        println("mobilId: $mobilId")
+                    }
+                )
+            }
         }
     }
 }
