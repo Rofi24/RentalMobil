0bb36a163a23774fc8ba7f171ec189386c2c3518
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-08 21:15:03 +0700

    Menambahkan fungsi composable untuk DestinasiHome.route dengan HomeScreen dan navigasi

--- app/src/main/java/com/example/rentalmobil/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/rentalmobil/navigation/PengelolaHalaman.kt
@@ -4,8 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.rentalmobil.ui.Halaman.DestinasiHome
+import com.example.rentalmobil.ui.Halaman.HomeScreen
+import com.example.rentalmobil.ui.add.DestinasiEntry
+import com.example.rentalmobil.ui.detail.DetailDestination
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
@@ -14,6 +18,16 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         startDestination = DestinasiHome.route,
         modifier = Modifier
     ){
-
+        composable(
+            DestinasiHome.route
+        ) {
+            HomeScreen(navigateToItemEntry = {
+                navController.navigate(DestinasiEntry.route)
+            },
+                onDetailClick = { itemId ->
+                    navController.navigate("${DetailDestination.route}/$itemId")
+                    println("itemId: $itemId")
+                })
+        }
     }
 }
