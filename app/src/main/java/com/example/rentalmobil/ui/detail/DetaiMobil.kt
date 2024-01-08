676b230bd6567307df7c3b3f7ff05cbaf94bb4ee
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-08 19:49:51 +0700

    Menambahkan komponen ItemDetailsRow

--- app/src/main/java/com/example/rentalmobil/ui/detail/DetaiMobil.kt
+++ app/src/main/java/com/example/rentalmobil/ui/detail/DetaiMobil.kt
@@ -1,10 +1,13 @@
 package com.example.rentalmobil.ui.detail
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import com.example.rentalmobil.navigation.DestinasiNavigasi
 
 object DetailDestination : DestinasiNavigasi {
@@ -12,6 +15,17 @@ object DetailDestination : DestinasiNavigasi {
     override val titleRes = "Detail Mobil"
     const val mobilId = "itemId"
     val routeWithArgs = "$route/{$mobilId}"
+}
+
+@Composable
+private fun ItemDetailsRow(
+    labelResID: String, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = labelResID, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
 }
 
 @Composable
