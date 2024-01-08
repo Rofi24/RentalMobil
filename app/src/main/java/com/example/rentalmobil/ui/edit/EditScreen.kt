a7864761ab6947e83fd014a64948faab2de07f98
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-08 20:42:31 +0700

    Menambahkan fungsi EditScreen untuk menampilkan tampilan edit mobil

--- app/src/main/java/com/example/rentalmobil/ui/edit/EditScreen.kt
+++ app/src/main/java/com/example/rentalmobil/ui/edit/EditScreen.kt
@@ -1,10 +1,60 @@
 package com.example.rentalmobil.ui.edit
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rentalmobil.navigation.DestinasiNavigasi
+import com.example.rentalmobil.ui.add.EntryBody
+import com.example.rentalmobil.ui.theme.PenyediaViewModel
+import com.example.rentalmobil.ui.theme.RentalTopAppBar
+import kotlinx.coroutines.launch
 
 object EditDestination : DestinasiNavigasi {
     override val route = "item_edit"
-    override val titleRes ="Edit Kontak"
+    override val titleRes ="Edit Mobil"
     const val mobilId = "itemId"
     val routeWithArgs = "${EditDestination.route}/{$mobilId}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            RentalTopAppBar(
+                title =EditDestination.titleRes,
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryBody(
+            addUIState = viewModel.rentalUiState,
+            onMobilValueChange = viewModel::updateUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateMobil()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
