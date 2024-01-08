8394abe2a6bd4b9b07a63126e6fcfe66e478a729
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-08 14:13:13 +0700

    Menambahkan komposisi Scaffold dan fungsi EntryBody dalam file [nama_file] untuk menampilkan Interface pengguna (UI) entri, termasuk top bar, body, dan fungsi-fungsi terkait dengan penambahan data mobil.

--- app/src/main/java/com/example/rentalmobil/ui/add/AddMobil.kt
+++ app/src/main/java/com/example/rentalmobil/ui/add/AddMobil.kt
@@ -1,19 +1,28 @@
 package com.example.rentalmobil.ui.add
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rentalmobil.navigation.DestinasiNavigasi
+import com.example.rentalmobil.ui.AddEvent
+import com.example.rentalmobil.ui.AddUIState
 import com.example.rentalmobil.ui.theme.PenyediaViewModel
+import com.example.rentalmobil.ui.theme.RentalTopAppBar
 import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi {
@@ -31,8 +40,41 @@ fun AddMobil(
 ) {
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            RentalTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
 
+        EntryBody(
+            addUIState = addViewModel.addUIState,
+            onSiswaValueChange = addViewModel::updateAddUIState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    addViewModel.addMobil()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
-
+@Composable
+fun EntryBody(
+    addUIState: AddUIState,
+    onSiswaValueChange: (AddEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
 }
 
