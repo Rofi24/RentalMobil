6071f86fec4f68e4b5b9c54d1f03ef0e8d593d66
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-08 19:45:16 +0700

    Menambahkan komponen DeleteConfirmationDialog

--- app/src/main/java/com/example/rentalmobil/ui/detail/DetaiMobil.kt
+++ app/src/main/java/com/example/rentalmobil/ui/detail/DetaiMobil.kt
@@ -1,5 +1,10 @@
 package com.example.rentalmobil.ui.detail
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.rentalmobil.navigation.DestinasiNavigasi
 
 object DetailDestination : DestinasiNavigasi {
@@ -7,4 +12,25 @@ object DetailDestination : DestinasiNavigasi {
     override val titleRes = "Detail Mobil"
     const val mobilId = "itemId"
     val routeWithArgs = "$route/{$mobilId}"
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text("Apakah anda yakin ingin menghapus data ini dari list?") },
+        text = { Text("Delete") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Tidak")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Ya")
+            }
+        }
+    )
 }
