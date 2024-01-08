9746e7dc48b1fd08ab618ca20fa74a6916fedd83
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-08 13:14:33 +0700

    Menambahkan (sealed class) 'RentalUIState' dengan tiga state: Success untuk hasil berhasil dengan aliran data mobil, Error untuk kesalahan, dan Loading untuk status sedang memuat.

--- app/src/main/java/com/example/rentalmobil/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/rentalmobil/ui/theme/Halaman/HalamanHome.kt
@@ -1,11 +1,17 @@
 package com.example.rentalmobil.ui.theme.Halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
 import com.example.rentalmobil.navigation.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi{
     override val route = "home"
     override val titleRes = "mobil"
 }
+
+
+
 
 
 
