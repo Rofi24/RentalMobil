5e102a9e6c890e16090ecdafb7e5a68c746404e3
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-08 12:58:50 +0700

    Menambahkan objek DestinasiHome yang mengimplementasikan Interface DestinasiNavigasi dengan rute 'home' dan judul 'mobil'.

--- app/src/main/java/com/example/rentalmobil/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/rentalmobil/ui/theme/Halaman/HalamanHome.kt
@@ -1,5 +1,11 @@
 package com.example.rentalmobil.ui.theme.Halaman
 
+import com.example.rentalmobil.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "mobil"
+}
 
 
 
