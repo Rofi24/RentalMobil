c94ad5d251f7864dd3da1b4e1ddd5e7dd4854dd5
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-08 21:12:04 +0700

    Menambahkan fungsi PengelolaHalaman dengan NavHost

---
+++ app/src/main/java/com/example/rentalmobil/navigation/PengelolaHalaman.kt
@@ -0,0 +1,19 @@
+package com.example.rentalmobil.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.rentalmobil.ui.Halaman.DestinasiHome
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+
+    }
+}
