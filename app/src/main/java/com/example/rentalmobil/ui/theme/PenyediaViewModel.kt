01cf2c964e018d9ecd7745aa264d31e28e7048b9
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-08 13:54:41 +0700

    Menambahkan objek PenyediaViewModel dengan properti Factory yang berisi inisialisasi untuk AddViewModel dan HomeViewModel menggunakan viewModelFactory. Inisialisasi dilakukan dengan mengakses repository melalui aplikasiRental().container.rentalRepository.

--- app/src/main/java/com/example/rentalmobil/ui/theme/PenyediaViewModel.kt
+++ app/src/main/java/com/example/rentalmobil/ui/theme/PenyediaViewModel.kt
@@ -1,8 +1,26 @@
 package com.example.rentalmobil.ui.theme
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.rentalmobil.ui.Halaman.HomeViewModel
 import com.example.rentalmobil.ui.RentalAplication
+import com.example.rentalmobil.ui.add.AddViewModel
 
 fun CreationExtras.aplikasiRental(): RentalAplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as RentalAplication)
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            AddViewModel(aplikasiRental().container.rentalRepository)
+        }
+
+        initializer {
+            HomeViewModel(aplikasiRental().container.rentalRepository)
+        }
+    }
+}
