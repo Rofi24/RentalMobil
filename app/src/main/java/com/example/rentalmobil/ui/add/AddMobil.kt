3a6e1d1106cd3c5e6d97f5c4683bd6e5a61cb8cb
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-08 13:57:34 +0700

    Membuat function AddMobil

--- app/src/main/java/com/example/rentalmobil/ui/add/AddMobil.kt
+++ app/src/main/java/com/example/rentalmobil/ui/add/AddMobil.kt
@@ -1,11 +1,36 @@
 package com.example.rentalmobil.ui.add
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rentalmobil.navigation.DestinasiNavigasi
+import com.example.rentalmobil.ui.theme.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Mobil"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddMobil(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    addViewModel: AddViewModel = viewModel(factory = PenyediaViewModel.Factory )
 
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+}
 
