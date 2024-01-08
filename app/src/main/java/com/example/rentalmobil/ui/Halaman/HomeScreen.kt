0022a0c3659de4e2361887bad3d5eae1c047cf07
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-08 13:43:31 +0700

    Membuat object DestinasiEntry dan membuat file AddMobil.kt

--- app/src/main/java/com/example/rentalmobil/ui/Halaman/HomeScreen.kt
+++ app/src/main/java/com/example/rentalmobil/ui/Halaman/HomeScreen.kt
@@ -1,9 +1,5 @@
 package com.example.rentalmobil.ui.Halaman
 
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rentalmobil.navigation.DestinasiNavigasi
 
 object DestinasiHome : DestinasiNavigasi{
---
+++ app/src/main/java/com/example/rentalmobil/ui/add/AddMobil.kt
@@ -0,0 +1,8 @@
+package com.example.rentalmobil.ui.add
+
+import com.example.rentalmobil.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mobil"
+}
