f954a0513511121107e6cfb4da6327104262b271
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-08 15:11:46 +0700

    Menambahkan card untuk DataMobil

--- app/src/main/java/com/example/rentalmobil/ui/Halaman/HomeScreen.kt
+++ app/src/main/java/com/example/rentalmobil/ui/Halaman/HomeScreen.kt
@@ -1,20 +1,34 @@
 package com.example.rentalmobil.ui.Halaman
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rentalmobil.model.Mobil
@@ -76,6 +90,104 @@ fun BodyHome(
     modifier: Modifier = Modifier,
     onMobilClick: (String) -> Unit = {}
 ) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemMobil.isEmpty()) {
+            Text(
+                text = "Tidak ada data Mobil",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        } else {
+            ListMobil(
+                itemMobil = itemMobil,
+                modifier = Modifier
+                    .padding(horizontal = 8.dp),
+                onItemClick = { onMobilClick(it.id) }
+            )
+        }
+    }
+}
+
+@Composable
+fun ListMobil(
+    itemMobil: List<Mobil>,
+    modifier: Modifier = Modifier,
+    onItemClick: (Mobil) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        this.items(itemMobil, key = { it.id }) { mobil ->
+            DataMobil(
+                mobil = mobil,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onItemClick(mobil) }
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+        }
+    }
+}
+
+@Composable
+fun DataMobil(
+    mobil: Mobil,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = mobil.merk,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = mobil.model,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = mobil.tahunProduksi,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(Modifier.weight(1f)
+            )
+            Text(
+                text = mobil.warna,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(Modifier.weight(1f)
+            )
+            Text(
+                text = mobil.platNomor,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(Modifier.weight(1f)
+            )
+            Text(
+                text = mobil.status,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(Modifier.weight(1f)
+            )
+        }
+    }
 }
 
 
