55b40be622a7d2af69b2a6f3e2a60b88dfbe2050
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-08 13:36:39 +0700

    Menambahkan class RentalAplication

---
+++ app/src/main/java/com/example/rentalmobil/ui/RentalAplication.kt
@@ -0,0 +1,15 @@
+package com.example.rentalmobil.ui
+
+import android.app.Application
+import com.example.rentalmobil.data.AppContainer
+import com.example.rentalmobil.data.RentalContainer
+
+class RentalAplication : Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+
+        container = RentalContainer()
+    }
+}
