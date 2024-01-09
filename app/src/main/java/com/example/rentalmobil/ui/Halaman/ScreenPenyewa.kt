76cc41762f336b83dcb0b6a2454225cbba841018
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-09 21:43:07 +0700

    Menambahkan komponen ListPenyewa dalam BodyHome untuk menampilkan daftar penyewa jika data tersedia.

--- app/src/main/java/com/example/rentalmobil/ui/Halaman/ScreenPenyewa.kt
+++ app/src/main/java/com/example/rentalmobil/ui/Halaman/ScreenPenyewa.kt
@@ -1,10 +1,13 @@
 package com.example.rentalmobil.ui.Halaman
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -97,6 +100,21 @@ fun BodyHome(
                 textAlign = TextAlign.Center,
                 style = MaterialTheme.typography.titleLarge
             )
+        } else {
+            ListPenyewa(
+                    itemPenyewa = itemPenyewa,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                ) { onPenyewaClick(it.id)
+            }
         }
     }
+}
+
+@Composable
+fun ListPenyewa(
+    itemPenyewa: List<Penyewa>,
+    modifier: Modifier = Modifier,
+    onItemClick: (Penyewa) -> Unit
+) {
 }
