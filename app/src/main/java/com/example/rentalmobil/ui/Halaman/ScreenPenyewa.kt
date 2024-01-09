c430ec1d8e04410c337bf97cf0b327dac29024a9
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-09 21:21:49 +0700

    Menambahkan function ScreenPenyewa dalam ScreenPenyewa.kt

--- app/src/main/java/com/example/rentalmobil/ui/Halaman/ScreenPenyewa.kt
+++ app/src/main/java/com/example/rentalmobil/ui/Halaman/ScreenPenyewa.kt
@@ -1,8 +1,21 @@
 package com.example.rentalmobil.ui.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rentalmobil.navigation.DestinasiNavigasi
+import com.example.rentalmobil.ui.PenyediaViewModel
 
 object DestinasiScreen : DestinasiNavigasi {
     override val route = "screen"
     override val titleRes = "Penyewa"
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScreenPenyewa(
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: PenyewaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {}
