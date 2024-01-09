9f80077b164abbab132303678037901b949d3d02
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-09 21:33:54 +0700

    Menambahkan tata letak (Scaffold, TopAppBar, FloatingActionButton, dan BodyHome) untuk halaman Penyewa dengan opsi untuk menambahkan penyewa

--- app/src/main/java/com/example/rentalmobil/ui/Halaman/ScreenPenyewa.kt
+++ app/src/main/java/com/example/rentalmobil/ui/Halaman/ScreenPenyewa.kt
@@ -1,12 +1,32 @@
 package com.example.rentalmobil.ui.Halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.rentalmobil.model.Penyewa
 import com.example.rentalmobil.navigation.DestinasiNavigasi
 import com.example.rentalmobil.ui.PenyediaViewModel
+import com.example.rentalmobil.ui.RentalTopAppBar
 
 object DestinasiScreen : DestinasiNavigasi {
     override val route = "screen"
@@ -21,4 +41,62 @@ fun ScreenPenyewa(
     viewModel: PenyewaViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            RentalTopAppBar(
+                title = "Penyewa",
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                FloatingActionButton(
+                    onClick = {},
+                    shape = MaterialTheme.shapes.medium
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = ""
+                    )
+                }
+                Spacer(modifier = Modifier.padding(8.dp))
+            }
+        }
+    ) { innerPadding ->
+        val uiStatePenyewa by viewModel.penyewaUIState.collectAsState()
+        BodyHome(
+            itemPenyewa = uiStatePenyewa.listPenyewa,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            onPenyewaClick = onDetailClick
+        )
+    }
+}
+
+@Composable
+fun BodyHome(
+    itemPenyewa: List<Penyewa>,
+    modifier: Modifier = Modifier,
+    onPenyewaClick: (String) -> Unit = {}
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemPenyewa.isEmpty()) {
+            Text(
+                text = "Tidak ada data Penyewa",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
 }
