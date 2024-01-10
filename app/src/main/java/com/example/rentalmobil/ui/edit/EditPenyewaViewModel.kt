cece94ff8a821f8d9a264f8ba3d458a6a7a55f61
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-10 21:58:27 +0700

    Menambahkan variabel penyewaUiState

--- app/src/main/java/com/example/rentalmobil/ui/edit/EditPenyewaViewModel.kt
+++ app/src/main/java/com/example/rentalmobil/ui/edit/EditPenyewaViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.rentalmobil.ui.edit
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.rentalmobil.data.PenyewaRepository
+import com.example.rentalmobil.ui.AddPenyewaUIState
 
 
 class EditPenyewaViewModel(
     savedStateHandle: SavedStateHandle,
     private val repository: PenyewaRepository
-) : ViewModel() {}
+) : ViewModel() {
+    var penyewaUiState by mutableStateOf(AddPenyewaUIState())
+        private set
+}
