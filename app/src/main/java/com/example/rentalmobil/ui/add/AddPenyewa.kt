d763c9af46ad2e45be4059545d2ad5a0a0245dd7
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-10 15:29:07 +0700

    Menambahkan objek DestinasiEntryPenyewa untuk item entry penyewa

---
+++ app/src/main/java/com/example/rentalmobil/ui/add/AddPenyewa.kt
@@ -0,0 +1,8 @@
+package com.example.rentalmobil.ui.add
+
+import com.example.rentalmobil.navigation.DestinasiNavigasi
+
+object DestinasiEntryPenyewa : DestinasiNavigasi {
+    override val route = "item_entry_penyewa"
+    override val titleRes = "Entry Penyewa"
+}
---
+++ app/src/main/java/com/example/rentalmobil/ui/add/AddPenyewaViewModel.kt
@@ -0,0 +1,2 @@
+package com.example.rentalmobil.ui.add
+
