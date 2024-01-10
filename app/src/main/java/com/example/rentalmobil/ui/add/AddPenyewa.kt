d37b4862e52df1eb77c9a237c95c174183bcb740
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-10 15:42:21 +0700

    Menambahkan fungsi FormInputPenyewa untuk input data penyewa

--- app/src/main/java/com/example/rentalmobil/ui/add/AddPenyewa.kt
+++ app/src/main/java/com/example/rentalmobil/ui/add/AddPenyewa.kt
@@ -1,8 +1,19 @@
 package com.example.rentalmobil.ui.add
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.rentalmobil.navigation.DestinasiNavigasi
+import com.example.rentalmobil.ui.AddEventPenyewa
 
 object DestinasiEntryPenyewa : DestinasiNavigasi {
     override val route = "item_entry_penyewa"
     override val titleRes = "Entry Penyewa"
 }
+
+@Composable
+fun FormInputPenyewa(
+    addEventPenyewa: AddEventPenyewa,
+    modifier: Modifier = Modifier,
+    onValueChange: (AddEventPenyewa) -> Unit = {},
+    enabled: Boolean = true
+) {}
