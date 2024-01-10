086a04c39775de01958fefb861f25750bf5c9a02
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-10 15:36:43 +0700

    Menambahkan class AddPenyewaViewModel untuk mengelola data Penyewa

--- app/src/main/java/com/example/rentalmobil/ui/add/AddPenyewaViewModel.kt
+++ app/src/main/java/com/example/rentalmobil/ui/add/AddPenyewaViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.rentalmobil.ui.add
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.rentalmobil.data.PenyewaRepository
+import com.example.rentalmobil.ui.AddEventPenyewa
+import com.example.rentalmobil.ui.AddPenyewaUIState
+import com.example.rentalmobil.ui.toPenyewa
+
+class AddPenyewaViewModel(private val penyewaRepository: PenyewaRepository) : ViewModel() {
+
+    var addPenyewaUIState by mutableStateOf(AddPenyewaUIState())
+        private set
+
+    fun updateAddPenyewaUIState(addEventPenyewa: AddEventPenyewa) {
+        addPenyewaUIState = AddPenyewaUIState(addEventPenyewa = addEventPenyewa)
+    }
+
+    suspend fun addPenyewa() {
+        penyewaRepository.save(addPenyewaUIState.addEventPenyewa.toPenyewa())
+    }
+}
