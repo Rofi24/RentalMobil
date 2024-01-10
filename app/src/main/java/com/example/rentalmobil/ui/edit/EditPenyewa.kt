898f5d107c4de628e7fb346d69aafbcf9959c8f9
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-10 22:12:43 +0700

    Menambahkan function EditPenyewa

--- app/src/main/java/com/example/rentalmobil/ui/edit/EditPenyewa.kt
+++ app/src/main/java/com/example/rentalmobil/ui/edit/EditPenyewa.kt
@@ -1,6 +1,11 @@
 package com.example.rentalmobil.ui.edit
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rentalmobil.navigation.DestinasiNavigasi
+import com.example.rentalmobil.ui.PenyediaViewModel
 
 object EditPenyewaDestination : DestinasiNavigasi {
     override val route = "item_edit_penyewa"
@@ -8,3 +13,12 @@ object EditPenyewaDestination : DestinasiNavigasi {
     const val penyewaId = "itemId_Penyewa"
     val routeWithArgs = "${EditPenyewaDestination.route}/{$penyewaId}"
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditPenyewa(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditPenyewaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {}
