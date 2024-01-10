f81191bbaf613b9bbac34db4aef0b12e37e26b64
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-10 16:33:04 +0700

    Menambahkan companion object TIMEOUT_MILLIS dan DetailPenyewaViewModel

--- app/src/main/java/com/example/rentalmobil/ui/detail/DetailPenyewaViewModel.kt
+++ app/src/main/java/com/example/rentalmobil/ui/detail/DetailPenyewaViewModel.kt
@@ -2,9 +2,34 @@ package com.example.rentalmobil.ui.detail
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.rentalmobil.data.PenyewaRepository
+import com.example.rentalmobil.ui.DetailUIStatePenyewa
+import com.example.rentalmobil.ui.toDetailPenyewa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class DetailPenyewaViewModel(
     savedStateHandle: SavedStateHandle,
     private val repository: PenyewaRepository
-) : ViewModel() {}
+) : ViewModel() {
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val penyewaId: String = checkNotNull(savedStateHandle[DetailDestinationPenyewa.penyewaId])
+
+    val uiState: StateFlow<DetailUIStatePenyewa> =
+        repository.getPenyewaById(penyewaId)
+            .filterNotNull()
+            .map {
+                DetailUIStatePenyewa(addEventPenyewa = it.toDetailPenyewa())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailUIStatePenyewa()
+            )
+}
