b26d791c24320150fd1ea24d43f2124d20077a9f
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-10 22:19:17 +0700

    Meimplementasikan fungsi komposabel EditPenyewa dengan kemampuan untuk kembali ke halaman sebelumnya, menangani navigasi ke atas, dan berinteraksi dengan EditPenyewaViewModel. Komposabel ini mencakup top app bar, body dengan penyewa UI state, dan fungsionalitas onSaveClick.

--- app/src/main/java/com/example/rentalmobil/ui/edit/EditPenyewa.kt
+++ app/src/main/java/com/example/rentalmobil/ui/edit/EditPenyewa.kt
@@ -1,11 +1,20 @@
 package com.example.rentalmobil.ui.edit
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.rentalmobil.navigation.DestinasiNavigasi
 import com.example.rentalmobil.ui.PenyediaViewModel
+import com.example.rentalmobil.ui.RentalTopAppBar
+import com.example.rentalmobil.ui.add.EntryBodyPenyewa
+import kotlinx.coroutines.launch
 
 object EditPenyewaDestination : DestinasiNavigasi {
     override val route = "item_edit_penyewa"
@@ -21,4 +30,31 @@ fun EditPenyewa(
     onNavigateUp: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EditPenyewaViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {}
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            RentalTopAppBar(
+                title =EditPenyewaDestination.titleRes,
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryBodyPenyewa(
+            addPenyewaUIState = viewModel.penyewaUiState,
+            onPenyewaValueChange = viewModel::updateUIStatePenyewa,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatePenyewa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
