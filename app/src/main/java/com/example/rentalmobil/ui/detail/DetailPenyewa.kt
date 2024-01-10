8da3bdfa6517ae884cc738d34460991bab01b49d
Irawan Rofi Hidayat <irawan.rofi.ft21@mail.umy.ac.id>
2024-01-10 16:39:15 +0700

    Menambahkan DeleteConfirmationDialog composable pada DetailPenyewa

--- app/src/main/java/com/example/rentalmobil/ui/detail/DetailPenyewa.kt
+++ app/src/main/java/com/example/rentalmobil/ui/detail/DetailPenyewa.kt
@@ -1,5 +1,10 @@
 package com.example.rentalmobil.ui.detail
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.rentalmobil.navigation.DestinasiNavigasi
 
 object DetailDestinationPenyewa : DestinasiNavigasi {
@@ -7,4 +12,25 @@ object DetailDestinationPenyewa : DestinasiNavigasi {
     override val titleRes = "Detail Penyewa"
     const val penyewaId = "itemId_penyewa"
     val routeWithArgs = "$route/{$penyewaId}"
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text("Apakah anda yakin ingin menghapus data penyewa ini dari list?") },
+        text = { Text("Hapus") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Tidak")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Ya")
+            }
+        }
+    )
 }
