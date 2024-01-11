7d4549f331fcb2d294052577221006d9e9a8e40b
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2024-01-11 20:51:14 +0700

    Membuat object DestinasiLogin untuk Login

---
+++ app/src/main/java/com/example/rentalmobil/ui/Halaman/LoginPage.kt
@@ -0,0 +1,9 @@
+package com.example.rentalmobil.ui.Halaman
+
+import com.example.rentalmobil.navigation.DestinasiNavigasi
+
+object DestinasiLogin : DestinasiNavigasi {
+    override val route = "login"
+    override val titleRes = "login_page"
+}
+
